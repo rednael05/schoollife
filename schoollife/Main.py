b49bafb6bf35befabd57d39da4a3a27385361e4f
Bonsch <markus.bonsch@instana.com>
2019-01-18 22:23:05 +0100

    anmeldung korregiert

--- schoollife/Main.py
+++ schoollife/Main.py
@@ -6,36 +6,41 @@ class Main:
     def __init__(self):
         self.datenbase = Database('benutzer-db.json')
 
-    # Two methods:
     def show(self):
-        auswahlAnmeldung = easygui.buttonbox("WILKOMMEN BEI SCHOOLLIFE", "SCHOOLLIFE", ["ANMELDEN", "REGISTRIEREN"])
-        if auswahlAnmeldung == "ANMELDEN":
-            anmeldedaten = easygui.multenterbox("ANMELDEN", "SCHOOLLIFE", ["NAME", "PASSWORT"])
-            user = self.datenbase.leseBenutzer(anmeldedaten[0])
-            if (user is None):  # User nicht in DB gefunden
-                easygui.msgbox("User exestiert nicht")
-            elif (user["passwort"] != anmeldedaten[1]):  # Passwort stimmt nicht mit DB überein
-                easygui.msgbox("Falsches Passwort")
-            else:
-                easygui.msgbox("Anmeldung war erfolgreich")
-        elif auswahlAnmeldung == "REGISTRIEREN":
-            registrierungsdaten = easygui.multenterbox("REGISTRIEREN", "SCHOOLLIFE",
-                                                       ["NACHNAME", "VORNAME", "E-MAIL ADRESSE", "PASSWORT",
-                                                        "PASSWORT BESTÄTIGEN"])
-            if registrierungsdaten[3] != registrierungsdaten[4]:
-                easygui.msgbox("PASWÖRTER STIMMEN NICHT ÜBEREIN")
-            elif self.datenbase.leseBenutzer(registrierungsdaten[2]):
-                easygui.msgbox("Benutzer mit email exestiert schon!")
-            else:
-                neuerUser = {
-                    "nachname": registrierungsdaten[0],
-                    "vorname": registrierungsdaten[1],
-                    "email": registrierungsdaten[2],
-                    "passwort": registrierungsdaten[3]
-                }
-            self.datenbase.leseBenutzer(neuerUser)
-            easygui.msgbox("Benutzer wurde erfolgreich angelegt!")
+        anmeldungAktiv = True
+
+        while anmeldungAktiv:
+            auswahlAnmeldung = easygui.buttonbox("WILKOMMEN BEI SCHOOLLIFE", "SCHOOLLIFE", ["ANMELDEN", "REGISTRIEREN"])
+            if auswahlAnmeldung == "ANMELDEN":
+                anmeldedaten = easygui.multenterbox("ANMELDEN", "SCHOOLLIFE", ["NAME", "PASSWORT"])
+                user = self.datenbase.leseBenutzer(anmeldedaten[0])
+                if (user is None):  # User nicht in DB gefunden
+                    easygui.msgbox("User exestiert nicht")
+                elif (user["passwort"] != anmeldedaten[1]):  # Passwort stimmt nicht mit DB überein
+                    easygui.msgbox("Falsches Passwort")
+                else:
+                    easygui.msgbox("Anmeldung war erfolgreich")
+                    break
+            elif auswahlAnmeldung == "REGISTRIEREN":
+                registrierungsdaten = easygui.multenterbox("REGISTRIEREN", "SCHOOLLIFE",
+                                                           ["NACHNAME", "VORNAME", "E-MAIL ADRESSE", "PASSWORT",
+                                                            "PASSWORT BESTÄTIGEN"])
+                if registrierungsdaten[3] != registrierungsdaten[4]:
+                    easygui.msgbox("PASWÖRTER STIMMEN NICHT ÜBEREIN")
+                elif self.datenbase.leseBenutzer(registrierungsdaten[2]) is not None:
+                    easygui.msgbox("Benutzer mit email exestiert schon!")
+                else:
+                    neuerUser = {
+                        "nachname": registrierungsdaten[0],
+                        "vorname": registrierungsdaten[1],
+                        "email": registrierungsdaten[2],
+                        "passwort": registrierungsdaten[3]
+                    }
+                    self.datenbase.speicherBenutzer(neuerUser)
+                    easygui.msgbox("Benutzer wurde erfolgreich angelegt!")
+
         self.datenbase.speichereDatenbank()
+
 
 if __name__ == "__main__":
     x = Main()
